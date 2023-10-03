dd4f1ab8e2b97d9c689934191939804b12cb03ff
Myat Su Mon <myatsumon2531999@gmail.com>
2023-10-03 16:25:05 +0630

    feat: custom validation

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -50,6 +50,18 @@ const YoutubeForm = () => {
                   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
                 message: "Invalid email format",
               },
+
+              validate: {
+                notAdmin: (fieldValue) => {
+                  return (
+                    fieldValue !== "admin@example.com" ||
+                    "Enter a different email address"
+                  );
+                },
+                notBlackListed: (fieldValue) => {
+                  return !fieldValue.endsWith("baddomain.com") || "This domain is not supported!"
+                }
+              }
             })}
           />
           <br />
